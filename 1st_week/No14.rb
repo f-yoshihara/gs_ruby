815014a10ea9be45ae01b77935636a6014947129
吉原　史朗 <>
2018-07-28 16:52:41 +0900

    ジーズlab rails講義　課題No14やりました

---
+++ 1st_week/No14.rb
@@ -0,0 +1,28 @@
+require 'active_record'
+# Loggerはログを記録するライブラリ。
+# ここではログの出力先をSTDOUT(標準出力に指定している)
+ActiveRecord::Base.logger = Logger.new(STDOUT)
+
+# usersというテーブルを扱うUserというモデルにActiveRecord::Baseを継承
+# これによりHeadlineにモデルとしての能力が与えられる
+# 接続とテーブル作成の段階では使わない
+class User < ActiveRecord::Base
+end
+
+# ここでDB接続を行う。
+ActiveRecord::Base.establish_connection(
+    adapter:  'mysql2',
+    host:     'localhost',
+    username: 'root',
+    password: '',
+    database: 'ruby_study',
+)
+
+# ActiveRecord::Migrationから継承。カラム指定。
+# 引数の二つ目はデータ型
+class CreateUsers < ActiveRecord::Migration[5.0]
+    create_table :users do |t|
+        t.column :gender, :string
+        t.column :age,    :integer
+    end
+end
